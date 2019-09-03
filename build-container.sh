dd498d090ef82f02fa83a69f98d062a7482e7e9b
youmych <youmych@gmail.com>
2019-09-03 10:03:20 +0300

    Maintaner can override user id and user name

--- build-container.sh
+++ build-container.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+USER_OVERRIDE=${1-}
+UID_OVERRIDE=${2-}
+
 docker build \
-	--build-arg VCS_REF=$(git rev-parse --short HEAD) \
-	--build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
-    --build-arg user=$USER \
-    --build-arg uid=$UID \
-	-t youmych/docker-ivesdk:latest .
+    --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+    --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+    --build-arg user=${USER_OVERRIDE:=$USER} \
+    --build-arg uid=${UID_OVERRIDE:=$UID} \
+    -t youmych/docker-ivesdk:latest .
